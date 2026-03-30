75e36583830e8bc3ef55dd68b633bebd39eb7006
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2026-03-30 14:45:10 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // frontend URL
+    origin: "https://myrealnotes.netlify.app", // frontend URL
     credentials: true,
   },
 });
