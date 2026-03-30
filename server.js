2cc5d05c04dc5b83f37b9c82f5a6afd7451e75f3
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2026-03-30 17:18:30 +0530

    Set up HTTP server with Socket.IO support

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 
+const http = require("http");
+const { Server } = require("socket.io");
+
+require("dotenv").config();
+
+const app = require("./app");
+
+const connection = require("./config/databaseConnection");
+
+const setupSocket = require("./socket/socket");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://notes-app-dun-beta.vercel.app/", // frontend URL
+    credentials: true,
+  },
+});
+
+setupSocket(io, connection);
+
+const WEBPORT = process.env.WEBPORT || 5000;
+
+server.listen(WEBPORT, () => {
+  console.log(`Web Server running on port ${WEBPORT}`);
+});
